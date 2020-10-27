16e26acd3fd808cbfa9be7ef0c6c901feabf7de8
kleban <yurakleban@gmail.com>
2020-10-27 10:56:05 +0200

    Initial Commit

---
+++ CourseDemoApplication/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CourseDemoApplication.MvcApplication" Language="C#" %>
---
+++ CourseDemoApplication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CourseDemoApplication.Startup))]
+namespace CourseDemoApplication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
